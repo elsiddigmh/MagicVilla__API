72590cc31a0fdf21e141e3beeb656e586e398cab
tchclaravel <tchc100@gmail.com>
2024-12-27 23:38:09 +0300

    Add Villa DTO to Villa Model

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas() {
+            return new List<VillaDTO>
+            {
+                new VillaDTO {Id= 1, Name = "Pool View"},
+                new VillaDTO {Id= 2, Name = "Beach View"}
+            };
+        }
+    }
+}
