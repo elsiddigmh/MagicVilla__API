cdff4e3adb5c7fb87f68572b1c7479398178737a
tchclaravel <tchc100@gmail.com>
2025-01-20 14:49:15 +0300

    Configure api versions

--- MagicVilla_VillaAPI/Controllers/UsersController.cs
+++ MagicVilla_VillaAPI/Controllers/UsersController.cs
@@ -6,8 +6,10 @@ using System.Net;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
-    [Route("api/UsersAuth")]
+    [Route("api/v{version:apiVersion}/UsersAuth")]
     [ApiController]
+    [ApiVersion("1.0")]
+
     public class UsersController : Controller
     {
         private readonly IUserRepository _userRepo;
--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -67,9 +67,11 @@ builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddApiVersioning(options => {
     options.AssumeDefaultVersionWhenUnspecified = true;
     options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.ReportApiVersions = true;
 });
 builder.Services.AddVersionedApiExplorer(options => {
     options.GroupNameFormat = "'v'VVV";
+    options.SubstituteApiVersionInUrl = true;
 });
 var key = builder.Configuration.GetValue<string>("ApiSettings:Secret");
 
